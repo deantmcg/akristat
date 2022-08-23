1ad027ce6a36d8e50f9608b61b9045190c859e52
Dean McGuigan <deantmcg@gmail.com>
2022-08-23 20:38:42 +0100

    Initial projects created

---
+++ AkriStat/AkriStat.Scrape/Services/FbRefSraper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AkriStat.Scrape.Services
+{
+    internal class FbRefSraper
+    {
+    }
+}
